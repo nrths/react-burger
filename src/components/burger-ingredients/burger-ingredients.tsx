5a4b6e1efcc2a9c308637517d64f22d87ec5d4d0
nrths <northnosouth@gmail.com>
2022-01-26 23:47:47 +0300

    before review step-1

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,10 +1,8 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import styles from "./burger-ingredients.module.css";
 import {
-  Tab,
-  CurrencyIcon,
-  Counter,
+  Tab
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import BurgerIngredient from '../burger-ingredient/burger-ingredient'
 import dataPropTypes from "../../utils/types";
@@ -15,43 +13,46 @@ function BurgerIngredients(props) {
   const sauces = props.data.filter((products) => products.type === "sauce");
   const mains = props.data.filter((products) => products.type === "main");
 
-//   const mainsList = useRef(null);
+  const mainsRef = useRef(null);
+  const saucesRef = useRef(null);
+  const bunsRef = useRef(null);
   
-//   const onTabClick = (productType) => {
-//     productType.current.scrollIntoView({block: "start", behavior: "smooth"});
-//   }
+  const onTabClick = (evt, ref) => {
+    setCurrent(evt);
+    ref.current.scrollIntoView({block: 'start', behavior: 'smooth'})
+  }
 
   return (
     <section>
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
 
       <div className={styles.tabs}>
-        <Tab value="bun" active={current === "bun"} onClick={setCurrent}>
+        <Tab value="bun" active={current === "bun"} onClick={evt => onTabClick(evt, bunsRef)}>
           Булки
         </Tab>
-        <Tab value="sauce" active={current === "sauce"} onClick={setCurrent}>
+        <Tab value="sauce" active={current === "sauce"} onClick={evt => onTabClick(evt, saucesRef)}>
           Соусы
         </Tab>
-        <Tab value="main" active={current === "main"} onClick={setCurrent}>
+        <Tab value="main" active={current === "main"} onClick={evt => onTabClick(evt, mainsRef)}>
           Начинки
         </Tab>
       </div>
       <div className={`${styles.ingredients_list} + mt-10 custom-scroll`}>
-        <h2 className="text text_type_main-medium">Булки</h2>
+        <h2 className="text text_type_main-medium" ref={bunsRef}>Булки</h2>
         <ul className={`${styles.products_list} + pr-4 pl-4 pt-6 pb-10`}>
           {buns.map((ingredient) => <li key={ingredient._id} className={`${styles.card}`}>
             <BurgerIngredient item={ingredient}/>
           </li>)}
         </ul>
 
-        <h2 className="text text_type_main-medium">Соусы</h2>
+        <h2 className="text text_type_main-medium" ref={saucesRef}>Соусы</h2>
         <ul className={`${styles.products_list} + pr-4 pl-4 pt-6 pb-10`}>
           {sauces.map((ingredient) => <li key={ingredient._id} className={`${styles.card}`}>
             <BurgerIngredient item={ingredient}/>
           </li>)}
         </ul>
 
-        <h2 className="text text_type_main-medium">Начинки</h2>
+        <h2 className="text text_type_main-medium" ref={mainsRef}>Начинки</h2>
         <ul className={`${styles.products_list} + pr-4 pl-4 pt-6 pb-10`}>
           {mains.map((ingredient) => <li key={ingredient._id} className={`${styles.card}`}>
             <BurgerIngredient item={ingredient}/>
