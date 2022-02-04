d253b5224c5d89e98412685ff5bf698410bfc3c9
nrths <northnosouth@gmail.com>
2022-02-04 17:59:54 +0300

    api-request ok

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { data } from "../../utils/data";
+import { baseUrl, checkResponse } from "../../utils/data";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <AppHeader />
-        <main className={styles.main}>
-          <BurgerIngredients data={data}/>
-          <BurgerConstructor data={data}/>
-        </main>
-      </>
-    );
-  }
-}
+const App = () => {
+
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+    hasError: false,
+  });
+
+  const getIngredients = () => {
+    fetch(`${baseUrl}`)
+    .then(checkResponse)
+    .then((res) => {
+      setState({...state, data: res.data});
+      console.log(res)})
+    .catch((error) => {
+      setState({...state, loading: false, hasError: true});
+      console.log(error);
+    })}
+    
+    useEffect(() => {
+      getIngredients();
+    }, []);
+
+  return (
+    <>
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients data={state.data} />
+        <BurgerConstructor data={state.data} />
+      </main>
+    </>
+  );
+};
 
 export default App;
