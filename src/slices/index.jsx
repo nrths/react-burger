d72b58d432d7826f067a1647142aab22a9c2896d
nrths <87331026+nrths@users.noreply.github.com>
2022-03-05 09:15:31 +0300

    Delete src/slices directory

--- src/slices/index.jsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import ingredientsReducer from './ingredients';
-
-const rootReducer = combineReducers({
-    ingredients: ingredientsReducer,
-})
-
-export default rootReducer;
